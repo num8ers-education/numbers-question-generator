92be2b75f244df3006d802f3f5db3353c6a2b524
ZaidRidha <zaidridha@yahoo.com>
2025-04-11 17:46:33 +0100

    Update SignupPage.tsx

--- FrontEnd/src/app/signup/SignupPage.tsx
+++ FrontEnd/src/app/signup/SignupPage.tsx
@@ -242,7 +242,7 @@ export default function SignupPage() {
             <p className="text-center text-sm text-gray-600">
               Already have an account?{" "}
               <Link
-                href="/login"
+                href="/auth/login"
                 className="font-medium text-blue-600 hover:text-blue-500">
                 Sign in
               </Link>
